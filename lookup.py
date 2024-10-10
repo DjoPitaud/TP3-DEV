65e2fbed6942eb62e475b2943dc4841f881cd1e6
unknown <pitaud.jonathan@gmail.com>
2024-10-10 21:58:09 +0200

    tp_dev

--- lookup.py
+++ lookup.py
@@ -3,11 +3,11 @@ import re
 from sys import argv
 
 def is_valid_domain(domain: str) -> bool:
-    # Expression régulière pour vérifier un nom de domaine valide : seulement des lettres minuscules et au moins un point
+    
     domain_pattern = re.compile(r"^[a-z0-9-]+\.[a-z]{2,}$")
     return bool(domain_pattern.match(domain))
 
-# Vérification si un argument est passé
+
 if len(argv) < 2:
     print("Veuillez fournir un nom de domaine.")
 else:
