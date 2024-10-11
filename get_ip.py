daf1e29ace70a864bea252e43cc2398daf353b4a
pitau <jonathan.pitaud@ynov.com>
2024-10-11 11:11:06 +0200

    tp

--- get_ip.py
+++ get_ip.py
@@ -6,15 +6,17 @@ def getNetworkInfo():
     
     nic_data = psutil.net_if_addrs()
     for nic_name in nic_data.keys():
-        if not nic_name == "Wi-Fi":
+        if nic_name == "Wi-Fi":
         
             for addr in nic_data[nic_name]:
                 if addr.family == AF_INET:
                     address = addr.address
                     netmask = addr.netmask
     cidr = sum(bin(int(x)).count('1') for x in netmask.split('.'))
+    nb_addr = 2^(32-cidr)
                 
     print(f"{address}/{cidr}")
+    print(f"{nb_addr} adresses")
     
 getNetworkInfo()
 
