a0aa8ac0ff60da60927206913c02004f831a3175
pitau <jonathan.pitaud@ynov.com>
2024-10-17 15:39:41 +0200

    tp

--- get_ip.py
+++ get_ip.py
@@ -11,8 +11,8 @@ def getIp():
             for addr in nic_data[nic_name]:
                 if addr.family == AF_INET:
                     address = addr.address
-                    netmask = addr.netmask
-    cidr = sum(bin(int(x)).count('1') for x in netmask.split('.'))
+                    masque_r = addr.netmask
+    cidr = sum(bin(int(x)).count('1') for x in masque_r.split('.'))
     nb_addr = 2^(32-cidr)
                 
     print(f"{address}/{cidr}")
