3cc4924bcf6da2f0d1de2df3a4a0841015e559d1
pitau <jonathan.pitaud@ynov.com>
2024-10-18 16:24:25 +0200

    test

--- tp4/bs_client_I1.py
+++ tp4/bs_client_I1.py
@@ -1,27 +1,20 @@
+from sys import exit as sysexit
 import socket
 
 def client():
     host = '192.168.56.102'  
     port = 13337               
 
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
     s.connect((host, port))
 
-
-
-    s.sendall("Meooooo !".encode())
-
-
+    s.sendall("Meooooo !".encode("UTF-8"))
     data = s.recv(1024)
 
-
     s.close()
-
-
     print(f"Le serveur a répondu {repr(data)}")
+    sysexit(0)
+    
 
 client()
 
