88df9b71aa774658a132428fffd698591c22ed48
pitau <jonathan.pitaud@ynov.com>
2024-10-22 14:17:58 +0200

    test

--- tp4/reponse.py
+++ tp4/reponse.py
@@ -1,7 +1,7 @@
 def repondre(message):
-    if b'meo' in message.lower():
+    if 'meo' in message.lower():
         return "Meo à toi confrère.".encode("UTF-8")
-    elif b'waf' in message.lower():
+    elif 'waf' in message.lower():
         return 'ptdr t ki'.encode("UTF-8")
     else:
         return "Mes respects humble humain.".encode("UTF-8")
