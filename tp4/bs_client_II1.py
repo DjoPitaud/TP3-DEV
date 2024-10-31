b7f8b55be2a7cb6ccd172952bece65d4ef5b7891
pitau <jonathan.pitaud@ynov.com>
2024-10-31 09:56:37 +0100

    on a bientot fini

---
+++ tp4/bs_client_II1.py
@@ -0,0 +1,82 @@
+from sys import exit as sysexit
+from os import path, makedirs 
+import socket
+import logging
+
+from re_verif import verifier_msg
+
+
+TEMP_DIR = "/tmp/bs_clientII"
+
+LOG_FILE = path.join(TEMP_DIR, "client.log")
+
+
+try:
+    makedirs(TEMP_DIR, exist_ok=True)
+except Exception:
+    print("Erreur lors de la création du dossier temporaire.")
+    sysexit(1)
+
+RED = "\033[91m"
+WHI = "\033[37m"
+NC = "\033[0m"
+
+logger = logging.getLogger("logs")
+logger.setLevel(20)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+logging.addLevelName(logging.ERROR, f"{RED}ERROR{NC}")
+logging.addLevelName(logging.INFO, f"{WHI}INFO{NC}")
+
+
+file_handler = logging.FileHandler(
+    "/tmp/bs_clientII/client.log", mode="a", encoding="utf-8"
+)
+logger.addHandler(file_handler)
+
+formatter = logging.Formatter(
+    "{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+def client():
+
+    host = '192.168.56.102'  
+    port = 13337         
+
+      
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect((host, port))
+        print(f"Connecté avec succès au serveur {host} sur le port {port}")
+        msg = str(input('Que veux-tu envoyer au serveur ? '))
+
+        if type(msg) is not str:
+            raise TypeError("Ici on veut que des strings !")
+        
+        elif verifier_msg(msg) is False:
+            raise ValueError("Ici on veut que meo et waf !")
+
+        
+        s.sendall(msg.encode("UTF-8"))
+        data = s.recv(1024).decode("UTF-8")
+        if not data: sysexit(1)
+                    
+
+        
+        print(f"Le serveur a répondu {repr(data)}")
+        sysexit(0)
+    except socket.error:
+        print("Error Occured.")
+        sysexit(2)
+    
+
+client()
+
--- tp4/bs_server_II1.py
+++ tp4/bs_server_II1.py
@@ -7,26 +7,28 @@ from reponse import repondre
 from connect_ip import connect_ip
 from connect_port import connect_port
 
-jaune = "\033[33m"
-blanc = "\033[37m"
-reset = "\033[0m"
+YEL = "\033[33m"
+WHI = "\033[37m"
+NC = "\033[0m"
 
 logger = logging.getLogger("logs")
 logger.setLevel(20)
-console_handler = logging.StreamHandler()  
+console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
 
-logging.addLevelName(logging.WARNING, f"{jaune}WARN{reset}")
-logging.addLevelName(logging.INFO, f"{blanc}INFO{reset}")
+logging.addLevelName(logging.WARNING, f"{YEL}WARN{NC}")
+logging.addLevelName(logging.INFO, f"{WHI}INFO{NC}")
 
 
-file_handler = logging.FileHandler("/var/log/bs_server/bs_server.log", mode="a", encoding="utf-8")
+file_handler = logging.FileHandler(
+    "/var/log/bs_server/bs_server.log", mode="a", encoding="utf-8"
+)
 logger.addHandler(file_handler)
 
 formatter = logging.Formatter(
     "{asctime} - {levelname} - {message}",
-        style="{",
-        datefmt="%Y-%m-%d %H:%M:%S",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 console_handler.setFormatter(formatter)
@@ -43,42 +45,41 @@ def main():
     s.bind((host, port))
 
     s.listen(1)
-    
+
     logger.info(f"Le serveur tourne sur {host}:{port}")
 
     start_time = time.time()
-    fin_timer = 60  
+    fin_timer = 60
 
     while True:
         try:
-            
+
             s.settimeout(1)
             try:
                 conn, addr = s.accept()
                 logger.info(f"Un client {addr[0]} s'est connecté.")
-                start_time = time.time()  
+                start_time = time.time()
             except socket.timeout:
-                
+
                 if time.time() - start_time >= fin_timer:
                     logger.warning("Aucun client depuis plus d'une minute.")
-                    start_time = time.time() 
+                    start_time = time.time()
                 continue
 
-            
             data = conn.recv(1024).decode("UTF-8")
             if not data:
                 logger.info("Le client a fermé la connexion.")
                 conn.close()
                 continue
-            
+
             logger.info(f"Le client {addr[0]} a envoyé {data}.")
-            
+
             response = repondre(data)
             conn.sendall(response)
             send_msg = response.decode("UTF-8")
             logger.info(f"Réponse envoyée au client <{addr[0]}> : {send_msg}.")
-            
-            conn.close() 
+
+            conn.close()
 
         except socket.error:
             logger.error("Une erreur est survenue avec la connexion.")
