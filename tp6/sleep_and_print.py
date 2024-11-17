f1f233d879375da843dd51f3775e68de20ddd26d
pitau <jonathan.pitaud@ynov.com>
2024-11-17 18:59:58 +0100

    tp6

--- tp6/sleep_and_print.py
+++ tp6/sleep_and_print.py
@@ -1,14 +1,9 @@
-import asyncio
+import time
 
-async def count_to_ten():
+def count_to_ten():
     for i in range(1, 11): 
         print(i)
-        await asyncio.sleep(0.5)  
+        time.sleep(0.5)
 
-
-async def main():
-    tasks = [count_to_ten(), count_to_ten() ]
-    await asyncio.gather(*tasks)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+count_to_ten()
+count_to_ten()      
