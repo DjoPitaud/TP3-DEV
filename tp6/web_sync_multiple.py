296c284f185c4ae3527c0e4f13d0d79a804878f4
pitau <jonathan.pitaud@ynov.com>
2024-11-18 06:25:21 +0100

    tp6

--- tp6/web_sync_multiple.py
+++ tp6/web_sync_multiple.py
@@ -2,6 +2,7 @@
 
 from os.path import exists
 from sys import exit as sysexit, argv
+import time
 
 from wrtite.write_content import write_content_in_file
 from get.get_content import get_content
@@ -10,6 +11,8 @@ from read.read_file import read_file
 
 def main():
     """Function printing python version."""
+
+    start_time = time.time()
 
     if not argv[1]:
         print("You must input a path (path/os/liste/urls)")
@@ -27,10 +30,13 @@ def main():
             content = get_content(url)
             output_file = f"/tmp/web_{url.replace('https://', '')}"
             write_content_in_file(content, output_file)
-            print(f"Content saved to: {output_file}")
+            
         except Exception as e:
             print(f"Failed to process URL {url}: {e}")
 
+    end_time = time.time() 
+    elapsed_time = end_time - start_time
+    print(f"\nTotal execution time: {elapsed_time:.2f} seconds")
 
 if __name__ == "__main__":
     main()
