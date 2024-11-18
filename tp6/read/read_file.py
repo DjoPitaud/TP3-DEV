10c1cdb8b0fcadd90779f336718cfb005d6cccef
pitau <jonathan.pitaud@ynov.com>
2024-11-18 05:29:52 +0100

    tp6

--- tp6/read/read_file.py
+++ tp6/read/read_file.py
@@ -2,17 +2,14 @@
 
 from sys import exit as sysexit
 
-path_file = "/home/djo/documents/liste_urls"
 
 def read_file(input_file):
     """Function printing python version."""
 
-    with open(input_file, "r") as file:
+    with open(input_file, "r", encoding="utf-8") as file:
         urls = [line.strip() for line in file if line.strip()]
-        print(f"{urls}")
-
+        cleaned_urls = [url.replace("https://", "") for url in urls]
+        return cleaned_urls
     if not urls:
         print(f"No URLs found in '{input_file}'. Exiting.")
         sysexit(1)
-
-read_file(path_file)
--- tp6/web_sync_multiple.py
+++ tp6/web_sync_multiple.py
@@ -1,23 +1,35 @@
 """Module providing a function printing python version."""
 
+from os.path import exists
 from sys import exit as sysexit, argv
 
 from wrtite.write_content import write_content_in_file
 from get.get_content import get_content
+from read.read_file import read_file
 
 
 def main():
     """Function printing python version."""
 
     if not argv[1]:
-        print("You must input a url(https://www.example.com)")
+        print("You must input a path (path/os/liste/urls)")
         sysexit(1)
 
-    url = argv[1]
-    output_file = "/tmp/web_page"
+    input_file = argv[1]
 
-    content = get_content(url)
-    write_content_in_file(content, output_file)
+    if not exists(input_file):
+        print(f"Error: File '{input_file}' does not exist.")
+        sysexit(1)
+    urls = read_file(input_file)
+
+    for url in urls:
+        try:
+            content = get_content(url)
+            output_file = f"/tmt/web_{url}"
+            write_content_in_file(content, output_file)
+            print(f"Content saved to: {output_file}")
+        except Exception as e:
+            print(f"Failed to process URL {url}: {e}")
 
 
 if __name__ == "__main__":
