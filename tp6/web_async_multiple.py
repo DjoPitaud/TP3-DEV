1bbbd42e0721f03973a9ba52f61d8c572b7a974d
pitau <jonathan.pitaud@ynov.com>
2024-11-18 06:47:59 +0100

    tp6

--- tp6/web_async_multiple.py
+++ tp6/web_async_multiple.py
@@ -4,12 +4,14 @@ from os.path import exists
 from sys import exit as sysexit, argv
 import time
 
-from wrtite.write_content import write_content_in_file
-from get.get_content import get_content
+import asyncio
+
+from wrtite.async_write_content import async_write_content_in_file
+from get.async_get_content import async_get_content
 from read.read_file import read_file
 
 
-def main():
+async def main():
     """Function printing python version."""
 
     start_time = time.time()
@@ -20,19 +22,21 @@ def main():
 
     input_file = argv[1]
 
-    if not exists(input_file):
+    if not await exists(input_file):
         print(f"Error: File '{input_file}' does not exist.")
         sysexit(1)
     urls = read_file(input_file)
 
     for url in urls:
         try:
-            content = get_content(url)
+            content = await async_get_content(url)
             output_file = f"/tmp/web_{url.replace('https://', '')}"
-            write_content_in_file(content, output_file)
+            await async_write_content_in_file(content, output_file)
 
         except Exception as e:
             print(f"Failed to process URL {url}: {e}")
+    
+    await asyncio.gather( exists, async_get_content, async_write_content_in_file)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -40,4 +44,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
