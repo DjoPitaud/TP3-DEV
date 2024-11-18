dd1474aba120b440d937947273874e9a972cd271
pitau <jonathan.pitaud@ynov.com>
2024-11-18 04:53:30 +0100

    tp6

---
+++ tp6/read/read_file.py
@@ -0,0 +1,18 @@
+"""Module providing a function printing python version."""
+
+from sys import exit as sysexit
+
+input_file = "home/djo/documents/liste_urls"
+
+def read_file(input_file):
+    """Function printing python version."""
+
+    with open(input_file, "r") as file:
+        urls = [line.strip() for line in file if line.strip()]
+        print(f"{urls}")
+
+    if not urls:
+        print(f"No URLs found in '{input_file}'. Exiting.")
+        sysexit(1)
+
+read_file()
