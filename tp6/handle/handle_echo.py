6e2913013ac9737c38c609429678694f9372f016
pitau <jonathan.pitaud@ynov.com>
2024-11-22 10:49:32 +0100

    creation server

---
+++ tp6/handle/handle_echo.py
@@ -0,0 +1,15 @@
+
+async def handle_echo(reader, writer):
+    data = await reader.read(1024)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+    await writer.wait_closed()
