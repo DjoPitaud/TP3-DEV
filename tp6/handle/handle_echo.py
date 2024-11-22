1ad4d322b4080b34015569e1815af3e9aa88d9b9
pitau <jonathan.pitaud@ynov.com>
2024-11-22 12:10:55 +0100

    new

--- tp6/handle/handle_echo.py
+++ tp6/handle/handle_echo.py
@@ -2,7 +2,7 @@
 async def handle_echo(reader, writer):
     data = await reader.read(1024)
     response = data.decode("UTF-8")
-    addr = writer.get_extra_info('client')
+    addr = writer.get_extra_info('peername')
 
     print(f"Received {response!r} from {addr!r}")
 
